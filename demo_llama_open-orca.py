4d402e29a37d941262390dbd0009687909e692c6
diixo <anatolii.podoba@gmail.com>
2025-07-11 09:04:34 +0300

    demo_llama_open-orca.py: working on GPU

--- demo_llama_open-orca.py
+++ demo_llama_open-orca.py
@@ -1,6 +1,6 @@
 
 import torch
-from transformers import AutoTokenizer, LlamaTokenizer, LlamaConfig, LlamaForCausalLM
+from transformers import AutoTokenizer, LlamaTokenizer, LlamaConfig, LlamaForCausalLM, AutoModelForCausalLM
 from datasets import load_dataset, Dataset
 from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
 from torch.utils.data import DataLoader
@@ -8,40 +8,22 @@ from torch.utils.data import DataLoader
 
 max_length=2048
 
-model_path = "./llama-160m"
+#model_path = "./llama-160m"
+model_path = "JackFram/llama-160m" # load from huggingface
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-def training_args():
-    params = torch.load(model_path + "/training_args.bin")
-    params = dict(params.__dict__)
-    for k, v in params.items():
-        print(f"{k}: {v}")
-#training_args()
-
+model = AutoModelForCausalLM.from_pretrained(model_path).to(device)
 tokenizer = AutoTokenizer.from_pretrained(model_path) # = LlamaTokenizer
+
 print(f"bos={tokenizer.bos_token_id}, pad={tokenizer.pad_token_id}, eos={tokenizer.eos_token_id}")
 
-#tokenizer.pad_token_id = tokenizer.eos_token_id
+if tokenizer.pad_token_id is None:
+    tokenizer.pad_token = tokenizer.eos_token
 
 
-# --- 2. Create config for LLaMA model
-config = LlamaConfig(
-    vocab_size=tokenizer.vocab_size,
-    hidden_size=512,            # 768
-    intermediate_size=2048,     # 3072
-    num_attention_heads=8,      # 12
-    num_hidden_layers=4,
-    max_position_embeddings=2048,
-    bos_token_id=tokenizer.bos_token_id,
-    eos_token_id=tokenizer.eos_token_id,
-    pad_token_id=tokenizer.pad_token_id,
-)
-
-# --- 3. Initialization from zero
-model = LlamaForCausalLM(config)
-
-
-# --- 4. Load dataset (streaming-mode, to avoid VRAM overload )
+# --- 4. Load dataset
 dataset = load_dataset("Open-Orca/OpenOrca", split="train", streaming=False)
 
 print(f"Size: {len(dataset)}")
@@ -55,7 +37,6 @@ def preprocess(example):
 
 dataset = dataset.map(preprocess, remove_columns=dataset.column_names)
 
-exit(0)
 
 
 # --- 4. Tokenization ---
@@ -81,6 +62,7 @@ def collate_fn(batch):
     )
 
     labels = input_ids.clone()
+    labels[labels == tokenizer.pad_token_id] = -100
 
     return {
         "input_ids": input_ids,
@@ -88,9 +70,33 @@ def collate_fn(batch):
         "labels": labels,
     }
 
-train_loader = DataLoader(
-    tokenized_dataset,
-    batch_size=2,
-    shuffle=True,
-    collate_fn=collate_fn
+#train_loader = DataLoader(tokenized_dataset, batch_size=2, shuffle=True, collate_fn=collate_fn)
+
+
+training_args = TrainingArguments(
+    output_dir="./llama_openorca_checkpoints",
+    overwrite_output_dir=True,
+    per_device_train_batch_size=8,
+    gradient_accumulation_steps=1,
+    num_train_epochs=1,
+    save_steps=100,
+    save_total_limit=2,
+    logging_steps=100,
+    learning_rate=5e-5,
+    weight_decay=0.005,
+    fp16=True,
+    optim="adamw_torch",
+    warmup_steps=0,
+    report_to="none",
+    eval_steps=None
 )
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset,
+    data_collator=collate_fn,
+)
+
+
+trainer.train()
